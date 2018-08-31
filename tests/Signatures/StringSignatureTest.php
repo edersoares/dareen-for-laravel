671b6e5133b075033199552a4f909f09ea2d30d2
Eder Soares <edersoares@me.com>
2018-08-30 23:01:11 -0300

    String signatures tests

---
+++ tests/Signatures/StringSignatureTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Signatures;
+
+use Dareen\Signatures\StringSignature;
+use Tests\TestCase;
+
+class StringSignatureTest extends TestCase
+{
+    public function testInstance()
+    {
+        $signature = new StringSignature('str_column');
+
+        $this->assertEquals('->string(\'str_column\')', (string) $signature);
+        $this->assertEquals('->string(\'str_column\')', $signature->sign());
+    }
+
+    public function testInstanceWithLenght()
+    {
+        $signature = new StringSignature('str_column', 10);
+
+        $this->assertEquals('->string(\'str_column\', 10)', (string) $signature);
+        $this->assertEquals('->string(\'str_column\', 10)', $signature->sign());
+    }
+}
