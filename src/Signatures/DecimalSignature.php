594dc64a1ff12f901224d31db770999c5a05c912
Eder Soares <edersoares@me.com>
2018-08-30 23:13:36 -0300

    Decimal signature

---
+++ src/Signatures/DecimalSignature.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Dareen\Signatures;
+
+class DecimalSignature extends AbstractSignature
+{
+    /**
+     * Signature name.
+     *
+     * @var string
+     */
+    protected $name = 'decimal';
+
+    /**
+     * DecimalSignature constructor.
+     *
+     * @param string $name
+     * @param int $precision
+     * @param int $scale
+     */
+    public function __construct($name, $precision = 8, $scale = 2)
+    {
+        $this->addArgument($name);
+
+        if ($this->isScaleShown($scale)) {
+            $this->addArgument($precision);
+            $this->addArgument($scale);
+        } elseif ($this->isPrecisionShown($precision)) {
+            $this->addArgument($precision);
+        }
+    }
+
+    /**
+     * Determine if precision is shown.
+     *
+     * @param int $precision
+     *
+     * @return bool
+     */
+    private function isPrecisionShown($precision)
+    {
+        return $precision > 0 && $precision != 8;
+    }
+
+    /**
+     * Determine if scale is shown.
+     *
+     * @param int $scale
+     *
+     * @return bool
+     */
+    private function isScaleShown($scale)
+    {
+        return $scale > 0 && $scale != 2;
+    }
+}
---
+++ tests/Signatures/DecimalSignatureTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Signatures;
+
+use Dareen\Signatures\DecimalSignature;
+use Tests\TestCase;
+
+class DecimalSignatureTest extends TestCase
+{
+    public function testInstance()
+    {
+        $signature = new DecimalSignature('decimal_column');
+
+        $this->assertEquals('->decimal(\'decimal_column\')', (string) $signature);
+        $this->assertEquals('->decimal(\'decimal_column\')', $signature->sign());
+    }
+
+    public function testInstanceWithPrecision()
+    {
+        $signature = new DecimalSignature('decimal_column', 10);
+
+        $this->assertEquals('->decimal(\'decimal_column\', 10)', (string) $signature);
+        $this->assertEquals('->decimal(\'decimal_column\', 10)', $signature->sign());
+    }
+
+    public function testInstanceWithPrecisionAndScale()
+    {
+        $signature = new DecimalSignature('decimal_column', 10, 4);
+
+        $this->assertEquals('->decimal(\'decimal_column\', 10, 4)', (string) $signature);
+        $this->assertEquals('->decimal(\'decimal_column\', 10, 4)', $signature->sign());
+    }
+}
