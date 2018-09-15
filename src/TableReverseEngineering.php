537b35de22691e06a4dd502c9ea3a1e4eff54072
Eder Soares <edersoares@me.com>
2018-09-15 19:58:19 -0300

    Table reverse engineering

---
+++ src/TableReverseEngineering.php
@@ -0,0 +1,194 @@
+<?php
+
+namespace Dareen;
+
+use Dareen\Definitions\TableDefinition;
+
+class TableReverseEngineering
+{
+    /**
+     * Indicate if columns definitions will returned.
+     *
+     * @var bool
+     */
+    private $columns = false;
+
+    /**
+     * Indicate if indexes definitions will returned.
+     *
+     * @var bool
+     */
+    private $indexes = false;
+
+    /**
+     * Indicate if foreign keys will returned.
+     *
+     * @var bool
+     */
+    private $foreignKeys = false;
+
+    /**
+     * Table definition.
+     *
+     * @var TableDefinition
+     */
+    private $table;
+
+    /**
+     * TableReverseEngineering constructor.
+     *
+     * @param TableDefinition $table
+     */
+    public function __construct(TableDefinition $table)
+    {
+        $this->table = $table;
+        $this->columns = true;
+        $this->indexes = true;
+        $this->foreignKeys = true;
+    }
+
+    /**
+     * Return if is a action to create table.
+     *
+     * @return bool
+     */
+    private function isCreateTableAction()
+    {
+        return $this->columns;
+    }
+
+    /**
+     * Return if is a action to add indexes on table.
+     *
+     * @return bool
+     */
+    private function isAddIndexesAction()
+    {
+        return $this->indexes
+            && $this->columns === false
+            && $this->foreignKeys === false;
+    }
+
+    /**
+     * Return if is a action to add foreign keys on table.
+     *
+     * @return bool
+     */
+    private function isAddForeignKeysAction()
+    {
+        return $this->foreignKeys
+            && $this->columns === false
+            && $this->indexes === false;
+    }
+
+    /**
+     * Skip the columns in migration.
+     *
+     * @return $this
+     */
+    public function skipColumns()
+    {
+        $this->columns = false;
+
+        return $this;
+    }
+
+    /**
+     * Skip the indexes in migration.
+     *
+     * @return $this
+     */
+    public function skipIndexes()
+    {
+        $this->indexes = false;
+
+        return $this;
+    }
+
+    /**
+     * Skip the foreign keys in migration.
+     *
+     * @return $this
+     */
+    public function skipForeignKeys()
+    {
+        $this->foreignKeys = false;
+
+        return $this;
+    }
+
+    /**
+     * Return the migrations definitions.
+     *
+     * @return array
+     */
+    public function getMigration()
+    {
+        $columns = [];
+        $indexes = [];
+        $foreign = [];
+
+        if ($this->columns) {
+            foreach ($this->table->getColumnsDefinitions() as $columnDefinition) {
+                $columns = array_merge($columns, $columnDefinition->getDefinition());
+            }
+        }
+
+        if ($this->indexes) {
+            foreach ($this->table->getIndexesDefinitions() as $indexDefinition) {
+                $indexes = array_merge($indexes, $indexDefinition->getDefinition());
+            }
+        }
+
+        if ($this->foreignKeys) {
+            foreach ($this->table->getForeignKeysDefinitions() as $foreignKeyDefinition) {
+                $foreign = array_merge($foreign, $foreignKeyDefinition->getDefinition());
+            }
+        }
+
+        $definitions = array_merge($columns, $indexes, $foreign);
+
+        return array_map(function ($definition) {
+            return '$table' . $definition . ';';
+        }, $definitions);
+    }
+
+    /**
+     * Return the migration class name.
+     *
+     * @return string
+     */
+    public function getMigrationClassName()
+    {
+        $name = $this->table->getTableName();
+
+        $name = str_replace('.', '_', $name);
+        $name = camel_case($name);
+        $name = ucfirst($name);
+
+        if ($this->isCreateTableAction()) {
+            $action = 'Create';
+        } elseif ($this->isAddForeignKeysAction()) {
+            $action = 'AddForeignKeysOn';
+        } elseif ($this->isAddIndexesAction()) {
+            $action = 'AddIndexesOn';
+        }  else {
+            $action = 'Alter';
+        }
+
+        return $action . $name . 'Table';
+    }
+
+    /**
+     * Return the migration filename.
+     *
+     * @return string
+     */
+    public function getMigrationFilename()
+    {
+        $date = date('Y_m_d_His');
+        $name = snake_case($this->getMigrationClassName());
+
+        return $date . '_' . $name . '.php';
+    }
+}
---
+++ tests/TableReverseEngineeringTest.php
@@ -0,0 +1,152 @@
+<?php
+
+namespace Tests;
+
+use Dareen\Definitions\TableDefinition;
+use Dareen\TableReverseEngineering;
+
+class TableReverseEngineeringTest extends TestCase
+{
+    private $table;
+
+    protected function setUp()
+    {
+        $this->table = $this->createMock(TableDefinition::class);
+    }
+
+    public function testSkipColumnsMethod()
+    {
+        $table = new TableReverseEngineering(
+            $this->table
+        );
+
+        $this->assertAttributeEquals(true, 'columns', $table);
+
+        $return = $table->skipColumns();
+
+        $this->assertEquals($table, $return);
+        $this->assertAttributeEquals(false, 'columns', $table);
+    }
+
+    public function testSkipIndexesMethod()
+    {
+        $table = new TableReverseEngineering(
+            $this->table
+        );
+
+        $this->assertAttributeEquals(true, 'indexes', $table);
+
+        $return = $table->skipIndexes();
+
+        $this->assertEquals($table, $return);
+        $this->assertAttributeEquals(false, 'indexes', $table);
+    }
+
+    public function testSkipForeignKeysMethod()
+    {
+        $table = new TableReverseEngineering(
+            $this->table
+        );
+
+        $this->assertAttributeEquals(true, 'foreignKeys', $table);
+
+        $return = $table->skipForeignKeys();
+
+        $this->assertEquals($table, $return);
+        $this->assertAttributeEquals(false, 'foreignKeys', $table);
+    }
+
+    public function testGetMigrationMethod()
+    {
+        $tableDefinition = $this->createMock(TableDefinition::class);
+
+        $tableDefinition->method('getColumnsDefinitions')->willReturn([]);
+        $tableDefinition->method('getIndexesDefinitions')->willReturn([]);
+        $tableDefinition->method('getForeignKeysDefinitions')->willReturn([]);
+
+        /** @var TableDefinition $tableDefinition */
+        $table = new TableReverseEngineering($tableDefinition);
+
+        $this->assertEquals(
+            [], $table->getMigration()
+        );
+    }
+
+    public function testGetMigrationClassNameMethod()
+    {
+        $tableDefinition = $this->createMock(TableDefinition::class);
+
+        $tableDefinition->method('getTableName')->willReturn('users');
+
+        /** @var TableDefinition $tableDefinition */
+        $table = new TableReverseEngineering($tableDefinition);
+
+        $this->assertEquals(
+            'CreateUsersTable', $table->getMigrationClassName()
+        );
+    }
+
+    public function testGetMigrationClassNameMethodAddForeignKeys()
+    {
+        $tableDefinition = $this->createMock(TableDefinition::class);
+
+        $tableDefinition->method('getTableName')->willReturn('users');
+
+        /** @var TableDefinition $tableDefinition */
+        $table = new TableReverseEngineering($tableDefinition);
+
+        $table->skipColumns();
+        $table->skipIndexes();
+
+        $this->assertEquals(
+            'AddForeignKeysOnUsersTable', $table->getMigrationClassName()
+        );
+    }
+
+    public function testGetMigrationClassNameMethodAddIndexes()
+    {
+        $tableDefinition = $this->createMock(TableDefinition::class);
+
+        $tableDefinition->method('getTableName')->willReturn('users');
+
+        /** @var TableDefinition $tableDefinition */
+        $table = new TableReverseEngineering($tableDefinition);
+
+        $table->skipColumns();
+        $table->skipForeignKeys();
+
+        $this->assertEquals(
+            'AddIndexesOnUsersTable', $table->getMigrationClassName()
+        );
+    }
+
+    public function testGetMigrationClassNameMethodAlterTable()
+    {
+        $tableDefinition = $this->createMock(TableDefinition::class);
+
+        $tableDefinition->method('getTableName')->willReturn('users');
+
+        /** @var TableDefinition $tableDefinition */
+        $table = new TableReverseEngineering($tableDefinition);
+
+        $table->skipColumns();
+
+        $this->assertEquals(
+            'AlterUsersTable', $table->getMigrationClassName()
+        );
+    }
+
+    public function testGetMigrationFilenameMethod()
+    {
+        $tableDefinition = $this->createMock(TableDefinition::class);
+
+        $tableDefinition->method('getTableName')->willReturn('users');
+
+        /** @var TableDefinition $tableDefinition */
+        $table = new TableReverseEngineering($tableDefinition);
+
+        $find = strpos($table->getMigrationFilename(), 'create_users_table.php');
+
+        $this->assertNotEmpty($find);
+    }
+}
