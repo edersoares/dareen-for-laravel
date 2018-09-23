f0287d4685a75ab7ce1492002540494b6e194b32
Eder Soares <edersoares@me.com>
2018-09-23 18:25:29 -0300

    Creates Migration class for use in tests (#1)

--- tests/Definitions/AbstractDefinition.php
+++ tests/Definitions/AbstractDefinition.php
@@ -2,51 +2,10 @@
 
 namespace Tests\Definitions;
 
-use Illuminate\Database\Connection;
+use Tests\Migration;
 
-abstract class AbstractDefinition
+abstract class AbstractDefinition extends Migration
 {
-    /**
-     * Schema builder for migration.
-     *
-     * @var \Illuminate\Database\Schema\Builder
-     */
-    protected $builder;
-
-    /**
-     * Schema manager for manipulate the database.
-     *
-     * @var \Doctrine\DBAL\Schema\AbstractSchemaManager
-     */
-    protected $manager;
-
-    /**
-     * Define the connection that will be used.
-     *
-     * @param Connection $connection
-     *
-     * @return void
-     */
-    public function setConnection(Connection $connection)
-    {
-        $this->builder = $connection->getSchemaBuilder();
-        $this->manager = $connection->getDoctrineConnection()->getSchemaManager();
-    }
-
-    /**
-     * Run the up method to migrate the database structure before test.
-     *
-     * @return void
-     */
-    abstract public function up();
-
-    /**
-     * Run the down method to reset migrate after test.
-     *
-     * @return void
-     */
-    abstract public function down();
-
     /**
      * Return the expected definition.
      *
