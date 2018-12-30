b2cf6ae7db36c48833575ddef17b34338a65c35e
Eder Soares <edersoares@me.com>
2018-12-30 12:18:16 -0200

    Test to drop foreign key signature

---
+++ tests/Signatures/DropForeignKeySignatureTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Signatures;
+
+use Dareen\Signatures\DropForeignKeySignature;
+use Tests\TestCase;
+
+class DropForeignKeySignatureTest extends TestCase
+{
+    public function testWithName()
+    {
+        $signature = new DropForeignKeySignature('posts_user_id_foreign');
+
+        $this->assertEquals('->dropForeign(\'posts_user_id_foreign\')', (string) $signature);
+        $this->assertEquals('->dropForeign(\'posts_user_id_foreign\')', $signature->sign());
+    }
+
+    public function testWithColumns()
+    {
+        $signature = new DropForeignKeySignature(['user_id', 'level_id']);
+
+        $this->assertEquals('->dropForeign([\'user_id\', \'level_id\'])', (string) $signature);
+        $this->assertEquals('->dropForeign([\'user_id\', \'level_id\'])', $signature->sign());
+    }
+}
